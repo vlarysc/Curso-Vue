ac59156ddbb1c42ec3c1a629348c7634fd50692f
Vlary <manoelfranciscose@gmail.com>
2021-01-04 18:07:02 -0300

    aula condicionais e listas

---
+++ condicionais-e-listas/desafio-01/app.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		logado: false,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		cursos: ['Vue.js', 'JavaScript', 'HTML e CSS3'],
+	}
+});
