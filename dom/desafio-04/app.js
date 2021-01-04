8eeeb463524ffb2486c127ff0f99a0e68a801a68
Vlary <manoelfranciscose@gmail.com>
2021-01-04 09:57:50 -0300

    aula Dom

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -5,7 +5,16 @@ new Vue({
 		classe2: "cor",
 		aplicarGirar: true,
 		aplicarClass: false,
-		classeCSS: 'cor1'
+		classeCSS: 'cor1',
+		cor: 'red',
+		classe3: '',
+		classe4: '',
+		cor1: '',
+		estilo2: {
+			width: '100px',
+			height: '100px'
+		},
+		width: '0'
 
 	},
 	methods: {
@@ -22,8 +31,20 @@ new Vue({
 				cor2: !this.aplicarClass
 			}
 		},
+		set(event) {
+			if (event.target.value == 'true') {
+				this.aplicarGirar = true
+			} else if (event.target.value == 'false') {
+				this.aplicarGirar = false
+			}
+		},
 		iniciarProgresso() {
-
+			let valor = 0
+			const temporizador = setInterval(() => {
+				valor += 5
+				this.width = `${valor}%`
+				if(valor == 100) clearInterval(temporizador)
+			}, 1000)			
 		}
 	}
 })
