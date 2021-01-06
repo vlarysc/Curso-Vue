271f8e1e9564e1b443caf84dff6301232677bc62
Vlary <manoelfranciscose@gmail.com>
2021-01-06 18:14:43 -0300

    Aula CLI

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app"> {{ titulo }} 
+    <button @click="titulo += '#$#'">Alterar</button>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data: function (){
+    return {
+      titulo: 'Testando Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: chocolate;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 50px;
+}
+</style>
